21210085098205f8e88e6c1961ffccdad65bc255
Shadows_of_Fire <Bward7864@gmail.com>
2019-02-08 00:05:15 -0500

    register tiles

--- src/main/java/cazador/furnaceoverhaul/FurnaceOverhaul.java
+++ src/main/java/cazador/furnaceoverhaul/FurnaceOverhaul.java
@@ -3,18 +3,23 @@ package cazador.furnaceoverhaul;
 import cazador.furnaceoverhaul.handler.GuiHandler;
 import cazador.furnaceoverhaul.init.ModObjects;
 import cazador.furnaceoverhaul.net.MessageSyncTE;
-import mcp.mobius.waila.proxy.IProxy;
+import cazador.furnaceoverhaul.tile.TileEntityDiamondFurnace;
+import cazador.furnaceoverhaul.tile.TileEntityEmeraldFurnace;
+import cazador.furnaceoverhaul.tile.TileEntityEndFurnace;
+import cazador.furnaceoverhaul.tile.TileEntityGoldFurnace;
+import cazador.furnaceoverhaul.tile.TileEntityIronFurnace;
+import cazador.furnaceoverhaul.tile.TileEntityZenithFurnace;
 import net.minecraft.creativetab.CreativeTabs;
 import net.minecraft.item.ItemStack;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.Mod.EventHandler;
 import net.minecraftforge.fml.common.Mod.Instance;
-import net.minecraftforge.fml.common.SidedProxy;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 import net.minecraftforge.fml.common.network.NetworkRegistry;
 import net.minecraftforge.fml.common.network.simpleimpl.SimpleNetworkWrapper;
+import net.minecraftforge.fml.common.registry.GameRegistry;
 import net.minecraftforge.fml.relauncher.Side;
 
 @Mod(modid = FurnaceOverhaul.MODID, name = FurnaceOverhaul.MODNAME, version = FurnaceOverhaul.VERSION)
@@ -27,9 +32,6 @@ public class FurnaceOverhaul {
 	@Instance
 	public static FurnaceOverhaul INSTANCE;
 
-	@SidedProxy(clientSide = "cazador.furnaceoverhaul.proxy.ClientProxy", serverSide = "cazador.furnaceoverhaul.proxy.ServerProxy")
-	public static IProxy proxy;
-
 	public static final SimpleNetworkWrapper NETWORK = NetworkRegistry.INSTANCE.newSimpleChannel(MODID);
 
 	@EventHandler
@@ -40,6 +42,12 @@ public class FurnaceOverhaul {
 
 	@EventHandler
 	public void init(FMLInitializationEvent event) {
+		GameRegistry.registerTileEntity(TileEntityIronFurnace.class, ModObjects.IRON_FURNACE.getRegistryName());
+		GameRegistry.registerTileEntity(TileEntityGoldFurnace.class, ModObjects.GOLD_FURNACE.getRegistryName());
+		GameRegistry.registerTileEntity(TileEntityDiamondFurnace.class, ModObjects.DIAMOND_FURNACE.getRegistryName());
+		GameRegistry.registerTileEntity(TileEntityEmeraldFurnace.class, ModObjects.EMERALD_FURNACE.getRegistryName());
+		GameRegistry.registerTileEntity(TileEntityEndFurnace.class, ModObjects.END_FURNACE.getRegistryName());
+		GameRegistry.registerTileEntity(TileEntityZenithFurnace.class, ModObjects.ZENITH_FURNACE.getRegistryName());
 	}
 
 	@EventHandler
